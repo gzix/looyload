7e25ed9d66c844d1975a8dd6a6fccca798f2aef8
Sungsit Sawaiwan <gibbozer@gmail.com>
2011-07-09 17:13:42 +0700

    Initail commit

---
+++ static/looyload.js
@@ -0,0 +1,27 @@
+
+  var activeColor = "#fff";
+  var disabledColor = "#999";
+  var defaultValue = "url ...";
+  
+  function loadBox() {
+    var box = document.getElementById("url_entry");
+    if (box.value != defaultValue) {
+      box.style.color = activeColor;
+    }
+  }
+  
+  function focusBox() {
+    var box = document.getElementById("url_entry");
+    if (box.value == defaultValue) {
+      box.value = "";
+      box.style.color = activeColor;
+    }
+  }
+  
+  function blurBox() {
+    var box = document.getElementById("url_entry");
+    if (box.value == "") {
+      box.style.color = disabledColor;
+      box.value = defaultValue;
+    }
+  }
